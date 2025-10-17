d62988f282b49de35570770a1dcd6aa560dedb12
Dmitry_work_ubuntu <tcheprasov.mitya@yandex.ru>
2025-10-17 15:22:32 +0300

    First templates and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+# вывод на страницу всех ОПУБЛИКОВАННЫХ постов
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+# Вывод информации о конкретном посте
+def post_detail(request, id):
+    """
+    данная строка заменяет блок try/except;
+    id=id, status=Post.Status.PUBLISHED - это именованные аргументы,
+    по которым происходит фильтрация
+    """
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
