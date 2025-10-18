435d57f3c511a08d233b4cb52c802bc9458759b6
Dmitry_work_ubuntu <tcheprasov.mitya@yandex.ru>
2025-10-18 14:40:03 +0300

    Added error handling for class Paginator()

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
@@ -15,7 +15,16 @@ def post_list(request):
     # то метод вернет значение по умолчанию – 1 (то есть, 
     # это будет первая страница)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 # Вывод информации о конкретном посте
