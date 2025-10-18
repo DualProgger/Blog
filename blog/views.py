73f9e776c30d12413858827d76756fae912fdeb2
Dmitry_work_ubuntu <tcheprasov.mitya@yandex.ru>
2025-10-18 14:33:38 +0300

    Add class Paginator for pages

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 
 # вывод на страницу всех ОПУБЛИКОВАННЫХ постов
 def post_list(request):
-    posts = Post.published.all()
+    # создаем объект класса Paginator с постраничной разбивкой
+    #  на 3 поста на каждой странице для всех 
+    # ОПУБЛИКОВАННЫХ постов all_posts 
+    all_posts = Post.published.all()
+    paginator = Paginator(all_posts, 3)
+    # метод, который пытается получить значение параметра page 
+    # из словаря request.GET. Если параметр page не найден, 
+    # то метод вернет значение по умолчанию – 1 (то есть, 
+    # это будет первая страница)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 # Вывод информации о конкретном посте
