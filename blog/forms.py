f60c6017da55013f3b5fcca4d27be87c2fbbab1e
Dmitry_work_ubuntu <tcheprasov.mitya@yandex.ru>
2025-10-23 15:28:55 +0300

    Create first class PostListView

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+"""
+Форма для отправки письма по его e-mail
+"""
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
 from .models import Post
 
 
-# вывод на страницу всех ОПУБЛИКОВАННЫХ постов
-def post_list(request):
-    # создаем объект класса Paginator с постраничной разбивкой
-    #  на 3 поста на каждой странице для всех 
-    # ОПУБЛИКОВАННЫХ постов all_posts 
-    all_posts = Post.published.all()
-    paginator = Paginator(all_posts, 3)
-    # метод, который пытается получить значение параметра page 
-    # из словаря request.GET. Если параметр page не найден, 
-    # то метод вернет значение по умолчанию – 1 (то есть, 
-    # это будет первая страница)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то
-        # выдать первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 # Вывод информации о конкретном посте
 def post_detail(request, year, month, day, slug):
@@ -42,3 +29,21 @@ def post_detail(request, year, month, day, slug):
                              publish__day=day,
                              slug=slug)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+# Обработка данных переданных из формы
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
